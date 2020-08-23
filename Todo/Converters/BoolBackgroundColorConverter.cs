e314137e032bf335a70762bb6ba829cdc9122dcb
swilliams29 <swillia47@rockets.utoledo.edu>
2020-08-23 10:59:15 -0500

    added converter

---
+++ Todo/Converters/BoolBackgroundColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Todo.Converters
+{
+    public class BoolBackgroundColorConverter : IValueConverter
+    {
+        public string TrueValue { get; set; }
+        public string FalseValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return FalseValue;
+            var arg = System.Convert.ToBoolean(value);
+            return arg ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
